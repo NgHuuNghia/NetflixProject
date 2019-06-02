169c71798826fac547e470b34f64ed966aa187a3
Nguyen Huu Nghia <you@example.com>
2019-06-02 14:06:45 +0700

    - Cập nhập database 02/06/2019 ở folder database - Thêm view adminMainWindow - Thêm NetflixModel Entites vào project để sử dụng

---
+++ Netflix_Project/Netflix/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Netflix.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool Isloaded = false;
+        public MainViewModel()
+        {
+            if (!Isloaded)
+            {
+                Isloaded = true;
+                AdminMainWindow adminMainWindow = new AdminMainWindow();
+                adminMainWindow.ShowDialog();
+            }   
+        }
+    }
+}
